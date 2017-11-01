15e013258d83e9a5af7096281887c092f84eca75
Jay <sikstrom@rapidtask.com>
2017-11-01 16:23:50 -0600

    Update views.py

--- rapid_task/users/api/views.py
+++ rapid_task/users/api/views.py
@@ -1,20 +1,20 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+# from django.contrib.auth.models import User, Group
+# from rest_framework import viewsets
 
-from .serializers import UserSerializer, GroupSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+# from .serializers import UserSerializer, GroupSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows groups to be viewed or edited.
+#     """
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer

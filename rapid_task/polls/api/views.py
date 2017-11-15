afd8adb707c15eb62a5e6b5bdafef5b7b1132cd3
theallseeingeye <sikstrom@rapidtask.com>
2017-11-15 06:11:28 -0700

    Created a new polls app. With the polls app I have created models for the polls, also created a serializer for all the models.
    -Still working progress on setting up the REST API

--- rapid_task/polls/api/views.py
+++ rapid_task/polls/api/views.py
@@ -2,37 +2,56 @@ from rapid_task.polls.models import Question, Poll, Feedback
 from rapid_task.polls.api.serializers import PollSerializer, QuestionSerializer, FeedbackSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
-from django.contrib.sessions.models import Session
 
 
-# Shows the list of feedbacks
 class FeedbackList(generics.ListCreateAPIView):
+    """
+    Shows the list of all the feedbacks that were received
+    """
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
 
 
-# Allow anyone to write the feedback
+
 class FeedbackCreate(generics.CreateAPIView):
+    """
+    Post:
+    Creates a new entry of feedback
+    """
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
     permission_classes = (AllowAny,)
 
 
-# Produces a list of created questions
 class QuestionList(generics.ListCreateAPIView):
+    """
+    Get:
+    Returns a list of all the poll answers to each question
+
+    Post:
+    Create a new question
+    *Nested relationship created* Create a new poll result
+    """
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     lookup_field = 'uuid'
 
     def create(self, validated_data):
+        # Pulls the "polls" data from the fields and deletes from the list- Then validates the data
         polls_data = validated_data.pop('polls')
+        # Pulls and validates the data and creates it
         questions = Question.objects.create(**validated_data)
+        # Saves the Question before saving the poll data to the question
         for poll_data in polls_data:
             Poll.objects.create(questions=questions, **poll_data)
         return questions
 
 
 class PollCreate(generics.CreateAPIView):
+    """
+    Post:
+    Creates the new poll result
+    """
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
     permission_classes = (AllowAny,)
@@ -59,7 +78,7 @@ class PollCreate(generics.CreateAPIView):
 # class PollDetail(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Poll.objects.all()
 #     serializer_class = PollSerializer
-        # This create query supports write-options to a relational nested serializer field.
+    # This create query supports write-options to a relational nested serializer field.
     # validated_data is data that has correctly changed from json to python native datatypes.
     # def create(self, validated_data):
     #     # .pop() is a python method of list objects. This removes the 'poll' data from list and returns it.

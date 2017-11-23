7da0fe3b250be62ed36a638f3148bf9c976a705a
theallseeingeye <sikstrom@rapidtask.com>
2017-11-23 09:48:31 -0700

    Testing if github is integrated here.

--- backend/config/settings/default.py
+++ backend/config/settings/default.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'website.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'website.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
